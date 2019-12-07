96a9a112cd5fdcdfb354d65045b39a6e1c4df57e
Alberto Irurueta <alberto@irurueta.com>
2019-12-07 01:46:22 +0100

    Added satellite position and velocity generator

---
+++ src/main/java/com/irurueta/navigation/gnss/SatellitePositionAndVelocityGenerator.java
@@ -0,0 +1,152 @@
+/*
+ * Copyright (C) 2019 Alberto Irurueta Carro (alberto@irurueta.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.irurueta.navigation.gnss;
+
+import com.irurueta.navigation.geodesic.Constants;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Computes satellites positions and velocities.
+ */
+public class SatellitePositionAndVelocityGenerator {
+
+    /**
+     * WGS84 Earth gravitational constant expressed in m^3 * s^-2
+     */
+    public static final double EARTH_GRAVITATIONAL_CONSTANT = Constants.EARTH_GRAVITATIONAL_CONSTANT;
+
+    /**
+     * Earth rotation rate expressed in radians per second (rad/s).
+     */
+    public static final double EARTH_ROTATION_RATE = Constants.EARTH_ROTATION_RATE;
+
+    /**
+     * Generates positions and velocities of satellites based on provided configuration.
+     *
+     * @param time   current time expressed in seconds (s).
+     * @param config GNSS configuration.
+     * @return collection containing position and velocities of satellites.
+     */
+    public static Collection<SatellitePositionAndVelocity> generateSatellitesPositionAndVelocity(
+            final double time, final GNSSConfig config) {
+        final List<SatellitePositionAndVelocity> result = new ArrayList<>();
+        generateSatellitesPositionAndVelocity(time, config, result);
+        return result;
+    }
+
+    /**
+     * Generates positions and velocities of satellites based on provided configuration.
+     *
+     * @param time   current time expressed in seconds (s).
+     * @param config GNSS configuration.
+     * @param result instance where computed positions and velocities of satellites will be stored.
+     */
+    public static void generateSatellitesPositionAndVelocity(final double time, final GNSSConfig config,
+                                                             final Collection<SatellitePositionAndVelocity> result) {
+        final int numSatellites = config.getNumberOfSatellites();
+        for (int j = 0; j < numSatellites; j++) {
+            final SatellitePositionAndVelocity satellitePositionAndVelocity = new SatellitePositionAndVelocity();
+            generateSatellitePositionAndVelocity(time, config, j, satellitePositionAndVelocity);
+            result.add(satellitePositionAndVelocity);
+        }
+    }
+
+    /**
+     * Generates position and velocity of a single satellite based on provided configuration.
+     *
+     * @param time   current time expressed in seconds (s).
+     * @param config GNSS configuration.
+     * @param j      number of satellite whose position and velocity must be computed.
+     * @return computed satellite position and velocity.
+     */
+    public static SatellitePositionAndVelocity generateSatellitePositionAndVelocity(final double time,
+                                                                                    final GNSSConfig config,
+                                                                                    final int j) {
+        final SatellitePositionAndVelocity result = new SatellitePositionAndVelocity();
+        generateSatellitePositionAndVelocity(time, config, j, result);
+        return result;
+    }
+
+    /**
+     * Generates position and velocity of a single satellite based on provided configuration.
+     *
+     * @param time   current time expressed in seconds (s).
+     * @param config GNSS configuration.
+     * @param j      number of satellite whose position and velocity must be computed.
+     * @param result instance where computed satellite position and velocity will be stored.
+     */
+    public static void generateSatellitePositionAndVelocity(final double time, final GNSSConfig config,
+                                                            final int j,
+                                                            final SatellitePositionAndVelocity result) {
+
+        // Convert inclination angle to radians.
+        final double inclinationRadians = Math.toRadians(config.getSatellitesInclinationDegrees());
+
+        // Determine orbital angular rate using (8.8)
+        final double orbitalRadius = config.getOrbitalRadiusOfSatellites();
+        final double orbitalRadius3 = orbitalRadius * orbitalRadius * orbitalRadius;
+        final double omegaIs = Math.sqrt(EARTH_GRAVITATIONAL_CONSTANT / orbitalRadius3);
+
+        // determine constellation time
+        final double constTime = time + config.getConstellationTimingOffset();
+
+        // (Corrected) argument of latitude
+        final double uOsO = 2.0 * Math.PI * j / config.getNumberOfSatellites()
+                + omegaIs * constTime;
+
+        // Satellite position in the orbital frame from (8.14)
+        final double cosUoso = Math.cos(uOsO);
+        final double sinUoso = Math.sin(uOsO);
+        final double rOsO1 = orbitalRadius * cosUoso;
+        final double rOsO2 = orbitalRadius * sinUoso;
+
+        // longitude of the ascending node from (8.16)
+        final double constDeltaLambdaRadians = Math.toRadians(config.getConstellationLongitudeOffsetDegrees());
+        final double omega = (Math.PI * ((j + 1) % 6) / 3.0 + constDeltaLambdaRadians)
+                - EARTH_ROTATION_RATE * constTime;
+
+        // ECEF satellite position from (8.19)
+        final double cosOmega = Math.cos(omega);
+        final double sinOmega = Math.sin(omega);
+        final double cosInclination = Math.cos(inclinationRadians);
+        final double sinInclination = Math.sin(inclinationRadians);
+
+        final double satelliteX = rOsO1 * cosOmega
+                - rOsO2 * cosInclination * sinOmega;
+        final double satelliteY = rOsO1 * sinOmega
+                + rOsO2 * cosInclination * cosOmega;
+        final double satelliteZ = rOsO2 * sinInclination;
+
+        // Satellite velocity in the orbital frame from (8.25), noting that with a circular orbit rOsO is
+        // constant and the time derivative of uOsO is omegaIs.
+        final double tmp = orbitalRadius * omegaIs;
+        final double vOsO1 = - tmp * sinUoso;
+        final double vOsO2 = tmp * cosUoso;
+
+        // ECEF satellite velocity from (8.26)
+        final double satelliteVx = vOsO1 * cosOmega - vOsO2 * cosInclination * sinOmega
+                + EARTH_ROTATION_RATE * satelliteY;
+        final double satelliteVy = vOsO1 * sinOmega + vOsO2 * cosInclination * cosOmega
+                - EARTH_ROTATION_RATE * satelliteX;
+        final double satelliteVz = vOsO2 * sinInclination;
+
+        result.setPositionCoordinates(satelliteX, satelliteY, satelliteZ);
+        result.setVelocityCoordinates(satelliteVx, satelliteVy, satelliteVz);
+    }
+}
---
+++ src/test/java/com/irurueta/navigation/gnss/SatellitePositionAndVelocityGeneratorTest.java
@@ -0,0 +1,209 @@
+/*
+ * Copyright (C) 2019 Alberto Irurueta Carro (alberto@irurueta.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.irurueta.navigation.gnss;
+
+import com.irurueta.navigation.geodesic.Constants;
+import com.irurueta.navigation.inertial.ECEFPosition;
+import com.irurueta.navigation.inertial.ECEFVelocity;
+import com.irurueta.statistics.UniformRandomizer;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Random;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class SatellitePositionAndVelocityGeneratorTest {
+
+    private static final double MIN_VALUE = 1e-4;
+    private static final double MAX_VALUE = 1e-3;
+
+    private static final int MIN_SATELLITES = 4;
+    private static final int MAX_SATELLITES = 10;
+
+    private static final double MIN_TIME_SECONDS = 0.0;
+    private static final double MAX_TIME_SECONDS = 10.0;
+
+    private static final double ABSOLUTE_ERROR = 1e-8;
+
+    @Test
+    public void testConstants() {
+        assertEquals(SatellitePositionAndVelocityGenerator.EARTH_GRAVITATIONAL_CONSTANT,
+                Constants.EARTH_GRAVITATIONAL_CONSTANT, 0.0);
+        assertEquals(SatellitePositionAndVelocityGenerator.EARTH_ROTATION_RATE, Constants.EARTH_ROTATION_RATE,
+                0.0);
+    }
+
+    @Test
+    public void testGenerateSatellitesPositionAndVelocity() {
+        final UniformRandomizer randomizer = new UniformRandomizer(new Random());
+        final double epochInterval = randomizer.nextDouble(MIN_VALUE, MAX_VALUE);
+        final double initialEstimatedEcefPositionX = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double initialEstimatedEcefPositionY = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double initialEstimatedEcefPositionZ = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final int numberOfSatellites = randomizer.nextInt(MIN_SATELLITES,
+                MAX_SATELLITES);
+        final double orbitalRadiusOfSatellites = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double satellitesInclinationDegrees = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double constellationLongitudeOffsetDegrees = randomizer.nextDouble(
+                MIN_VALUE, MAX_VALUE);
+        final double constellationTimingOffset = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double maskAngleDegrees = randomizer.nextDouble(MIN_VALUE, MAX_VALUE);
+        final double sisErrorSD = randomizer.nextDouble(MIN_VALUE, MAX_VALUE);
+        final double zenithIonosphereErrorSD = randomizer.nextDouble(
+                MIN_VALUE, MAX_VALUE);
+        final double zenithTroposphereErrorSD = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double codeTrackingErrorSD = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double rangeRateTrackingErrorSD = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double initialReceiverClockOffset = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double initialReceiverClockDrift = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+
+        final GNSSConfig config = new GNSSConfig(epochInterval, initialEstimatedEcefPositionX,
+                initialEstimatedEcefPositionY, initialEstimatedEcefPositionZ,
+                numberOfSatellites, orbitalRadiusOfSatellites,
+                satellitesInclinationDegrees, constellationLongitudeOffsetDegrees,
+                constellationTimingOffset, maskAngleDegrees, sisErrorSD,
+                zenithIonosphereErrorSD, zenithTroposphereErrorSD, codeTrackingErrorSD,
+                rangeRateTrackingErrorSD, initialReceiverClockOffset,
+                initialReceiverClockDrift);
+
+        final double time = randomizer.nextDouble(MIN_TIME_SECONDS, MAX_TIME_SECONDS);
+
+        final List<SatellitePositionAndVelocity> result1 = new ArrayList<>();
+        for (int j = 0; j < numberOfSatellites; j++) {
+            result1.add(computeSatellitePositionAndVelocity(time, config, j));
+        }
+
+        final Collection<SatellitePositionAndVelocity> result2 =
+                SatellitePositionAndVelocityGenerator
+                        .generateSatellitesPositionAndVelocity(time, config);
+        final Collection<SatellitePositionAndVelocity> result3 = new ArrayList<>();
+        SatellitePositionAndVelocityGenerator.generateSatellitesPositionAndVelocity(
+                time, config, result3);
+
+        assertEquals(result1, result2);
+        assertEquals(result1, result3);
+    }
+
+    @Test
+    public void testGenerateSatellitePositionAndVelocity() {
+        final UniformRandomizer randomizer = new UniformRandomizer(new Random());
+        final double epochInterval = randomizer.nextDouble(MIN_VALUE, MAX_VALUE);
+        final double initialEstimatedEcefPositionX = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double initialEstimatedEcefPositionY = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double initialEstimatedEcefPositionZ = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final int numberOfSatellites = randomizer.nextInt(MIN_SATELLITES,
+                MAX_SATELLITES);
+        final double orbitalRadiusOfSatellites = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double satellitesInclinationDegrees = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double constellationLongitudeOffsetDegrees = randomizer.nextDouble(
+                MIN_VALUE, MAX_VALUE);
+        final double constellationTimingOffset = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double maskAngleDegrees = randomizer.nextDouble(MIN_VALUE, MAX_VALUE);
+        final double sisErrorSD = randomizer.nextDouble(MIN_VALUE, MAX_VALUE);
+        final double zenithIonosphereErrorSD = randomizer.nextDouble(
+                MIN_VALUE, MAX_VALUE);
+        final double zenithTroposphereErrorSD = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double codeTrackingErrorSD = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double rangeRateTrackingErrorSD = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double initialReceiverClockOffset = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+        final double initialReceiverClockDrift = randomizer.nextDouble(MIN_VALUE,
+                MAX_VALUE);
+
+        final GNSSConfig config = new GNSSConfig(epochInterval, initialEstimatedEcefPositionX,
+                initialEstimatedEcefPositionY, initialEstimatedEcefPositionZ,
+                numberOfSatellites, orbitalRadiusOfSatellites,
+                satellitesInclinationDegrees, constellationLongitudeOffsetDegrees,
+                constellationTimingOffset, maskAngleDegrees, sisErrorSD,
+                zenithIonosphereErrorSD, zenithTroposphereErrorSD, codeTrackingErrorSD,
+                rangeRateTrackingErrorSD, initialReceiverClockOffset,
+                initialReceiverClockDrift);
+
+        final double time = randomizer.nextDouble(MIN_TIME_SECONDS, MAX_TIME_SECONDS);
+
+        for (int j = 0; j < numberOfSatellites; j++) {
+            final SatellitePositionAndVelocity result1 = new SatellitePositionAndVelocity();
+            SatellitePositionAndVelocityGenerator.generateSatellitePositionAndVelocity(time, config, j, result1);
+            final SatellitePositionAndVelocity result2 = SatellitePositionAndVelocityGenerator
+                    .generateSatellitePositionAndVelocity(time, config, j);
+
+            final SatellitePositionAndVelocity result3 = computeSatellitePositionAndVelocity(time, config, j);
+
+            assertTrue(result1.equals(result3, ABSOLUTE_ERROR));
+            assertTrue(result2.equals(result3, ABSOLUTE_ERROR));
+        }
+    }
+
+    private SatellitePositionAndVelocity computeSatellitePositionAndVelocity(final double time,
+                                                                             final GNSSConfig config, final int j) {
+        final double inclination = Math.toRadians(config.getSatellitesInclinationDegrees());
+
+        final double omegaIs = Math.sqrt(Constants.EARTH_GRAVITATIONAL_CONSTANT /
+                Math.pow(config.getOrbitalRadiusOfSatellites(), 3.0));
+
+        final double constTime = time + config.getConstellationTimingOffset();
+
+        final double uoso = 2.0 * Math.PI * j / config.getNumberOfSatellites() + omegaIs * constTime;
+
+        final double[] roso = new double[]{ config.getOrbitalRadiusOfSatellites() * Math.cos(uoso),
+                config.getOrbitalRadiusOfSatellites() * Math.sin(uoso) };
+
+        final double omega = Math.PI * ((j + 1) % 6) / 3
+                + Math.toRadians(config.getConstellationLongitudeOffsetDegrees())
+                - Constants.EARTH_ROTATION_RATE * constTime;
+
+        final ECEFPosition position = new ECEFPosition(
+                roso[0] * Math.cos(omega) - roso[1] * Math.cos(inclination) * Math.sin(omega),
+                roso[0] * Math.sin(omega) + roso[1] * Math.cos(inclination) * Math.cos(omega),
+                roso[1] * Math.sin(inclination));
+
+        final double[] voso = new double[] { - config.getOrbitalRadiusOfSatellites() * omegaIs * Math.sin(uoso),
+                config.getOrbitalRadiusOfSatellites() * omegaIs * Math.cos(uoso), 0.0 };
+
+        final ECEFVelocity velocity = new ECEFVelocity(
+                voso[0] * Math.cos(omega) - voso[1] * Math.cos(inclination) * Math.sin(omega)
+                        + Constants.EARTH_ROTATION_RATE * position.getY(),
+                voso[0] * Math.sin(omega) + voso[1] * Math.cos(inclination) * Math.cos(omega)
+                        - Constants.EARTH_ROTATION_RATE * position.getX(),
+                voso[1] * Math.sin(inclination));
+
+        return new SatellitePositionAndVelocity(position, velocity);
+    }
+}
