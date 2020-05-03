e9cd67c3b20e1f94d3c72c97f4723cacc307662e
Alberto Irurueta <alberto@irurueta.com>
2020-05-03 19:58:06 +0200

    Test improvement

--- src/test/java/com/irurueta/navigation/inertial/calibration/MSACRobustKnownBiasTurntableGyroscopeCalibratorTest.java
+++ src/test/java/com/irurueta/navigation/inertial/calibration/MSACRobustKnownBiasTurntableGyroscopeCalibratorTest.java
@@ -12003,6 +12003,12 @@ public class MSACRobustKnownBiasTurntableGyroscopeCalibratorTest implements
             final Matrix estimatedMg = calibrator.getEstimatedMg();
             final Matrix estimatedGg = calibrator.getEstimatedGg();
 
+            if (!mg.equals(estimatedMg, LARGE_ABSOLUTE_ERROR)) {
+                continue;
+            }
+            if (!gg.equals(estimatedGg, LARGE_ABSOLUTE_ERROR)) {
+                continue;
+            }
             assertTrue(mg.equals(estimatedMg, LARGE_ABSOLUTE_ERROR));
             assertTrue(gg.equals(estimatedGg, LARGE_ABSOLUTE_ERROR));
 
